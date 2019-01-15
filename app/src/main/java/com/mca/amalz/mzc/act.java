933ebf11e6b52276af9ae08620ee48169413af2d
amalraj1264 <amalraj1264@gmail.com>
2019-01-15 11:45:45 +0530

    button click completed

--- app/src/main/java/com/mca/amalz/mzc/act.java
+++ app/src/main/java/com/mca/amalz/mzc/act.java
@@ -2,12 +2,32 @@ package com.mca.amalz.mzc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class act extends AppCompatActivity {
-
+EditText ed1,ed2;
+Button but;
+String getUSN,getPASS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act);
+        ed1 = (EditText) findViewById(R.id.usn);
+        ed2 = (EditText) findViewById(R.id.pass);
+        but = (Button) findViewById(R.id.loginbut);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUSN=ed1.getText().toString();
+                getPASS=ed2.getText().toString();
+//                Toast.makeText(getApplicationContext(),"Hello World",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),getUSN,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),getPASS,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
